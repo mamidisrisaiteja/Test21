f39b716a54d4fbd06ae956c8c0577b615916a994
AN574BV <AN574BV@IN3072595W1.ey.net>
2022-07-19 13:02:57 +0530

    Test2

--- MvnTestTejaToJasmiDemo21/src/test/java/cucumber/options/TestRunner1.java
+++ MvnTestTejaToJasmiDemo21/src/test/java/cucumber/options/TestRunner1.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\AN574BV\\eclipse-workspace\\MvnTestTejaToJasmiDemo21\\src\\test\\java\\features",glue= {"stepSefinitions"},plugin= "html:target/cucumber-reports/CucumberTestReport.html")
+@CucumberOptions(features="C:\\Users\\AN574BV\\eclipse-workspace\\MvnTestTejaToJasmiDemo21\\src\\test\\java\\features",glue= {"stepSefinitions"},plugin= "json:reports/cucumber-reports/CucumberTestReport.html")
 public class TestRunner1 {
 	
 	
